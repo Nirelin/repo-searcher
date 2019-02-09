8a9082c0df1b77d9570f4d595c33c2daec30b8ec
Nirelin <31349091+Nirelin@users.noreply.github.com>
2019-02-09 22:26:46 +0200

    Initial dist subtree commit

---
+++ build/precache-manifest.83096efa771a3beb22c8d33a7bbb2202.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b384abb4b083d6d8ab13",
+    "url": "/static/js/main.b384abb4.chunk.js"
+  },
+  {
+    "revision": "2dffd56a7ba56a382ab6",
+    "url": "/static/js/1.2dffd56a.chunk.js"
+  },
+  {
+    "revision": "b384abb4b083d6d8ab13",
+    "url": "/static/css/main.602fe174.chunk.css"
+  },
+  {
+    "revision": "2dffd56a7ba56a382ab6",
+    "url": "/static/css/1.1b56aaf1.chunk.css"
+  },
+  {
+    "revision": "d1d95b85c4c94131f9890fc525e35b15",
+    "url": "/index.html"
+  }
+];
